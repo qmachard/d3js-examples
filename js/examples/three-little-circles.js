3f1d902c5a3ae703ec5e1e1500ec7526ccf0d4b1
Quentin MACHARD <quentin.machard@niji.fr>
2017-09-11 16:09:03 +0200

    Example 5 - Final

--- js/examples/three-little-circles.js
+++ js/examples/three-little-circles.js
@@ -1,10 +1,18 @@
 var svg = d3.select('svg');
 
-var circle = svg.selectAll("circle")
-    .data([32, 57]);
+var circle = svg.selectAll('circle')
+    .data([32, 57, 293, 567, 34], function(d) {
+        return d;
+    });
 
-circle.style("fill", "steelblue");
-circle.attr("r", function(d) { return Math.sqrt(d); });
-circle.attr("cx", function(d, i) { return i * 100 + 30; });
+circle.enter().append('circle')
+    .attr('cy', 60)
+    .attr('cx', function(d, i) {
+        return i * 100 + 30;
+    })
+    .attr('r', function(d) {
+        return Math.sqrt(d);
+    })
+;
 
 circle.exit().remove();
