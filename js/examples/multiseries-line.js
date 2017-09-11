71e8765fee4708cc4d2a8b0c1c4e8ef4295b9265
Quentin MACHARD <quentin.machard@niji.fr>
2017-09-11 17:50:40 +0200

    Feat. Enable zoom to multi-lines chart

--- js/examples/multiseries-line.js
+++ js/examples/multiseries-line.js
@@ -4,29 +4,48 @@ var height = 400,
     colors = ['#008080', '#5C9DE6', '#125EB3', '#8D3B23', '#B31E12']
 ;
 
+// Setup a DateTime parser
+var parseTime = d3.timeParse("%Y-%m");
+
 // Create SVG
 var svg = d3
     .select('#chart')
     .append('svg')
     .attr('width', '100%')
-    .attr('height', '100%')
     .attr('viewBox', '0 0 ' + width + ' ' + height)
-    .attr('preserveAspectRatio', 'xMinYMin')
+    .attr('preserveAspectRatio', 'xMaxYMin')
 ;
+
+width = width - margin.left - margin.right;
+height = height - margin.top - margin.bottom;
+
+svg.append("defs").append("clipPath")
+    .attr("id", "clip")
+    .append("rect")
+    .attr("width", width)
+    .attr("height", height);
 
 // Create container
 var graph = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 ;
 
-width = width - margin.left - margin.right;
-height = height - margin.top - margin.bottom;
-
-
-// Create scales
+// Setup scales
 var x = d3.scaleTime().range([0, width]),
     y = d3.scaleLinear().range([height, 0]),
     z = d3.scaleOrdinal(d3.schemeCategory10);
+
+// Setup Axis
+var xAxis = d3.axisBottom(x),
+    yAxis = d3.axisLeft(y);
+
+// Setup zoom & area
+var zoom = d3.zoom()
+    .scaleExtent([1, 32])
+    .translateExtent([[0, 0], [width, height]])
+    .extent([[0, 0], [width, height]])
+    .on('zoom', onZoom)
+;
 
 // Setup data binding on line
 var line = d3.line()
@@ -38,8 +57,26 @@ var line = d3.line()
         return y(d.number);
     });
 
-// Setup a DateTime parser
-var parseTime = d3.timeParse("%Y-%m");
+function onZoom() {
+    var t = d3.event.transform,
+        xt = t.rescaleX(x)
+    ;
+
+    // Method to rescale
+    var zoomedLine = line.x(function(d) {
+       return xt(d.date);
+    });
+
+    graph.selectAll(".line")
+        .attr("d", function(d) {
+            return zoomedLine(d.values);
+        })
+    ;
+
+    graph.select(".axis--x")
+        .call(xAxis.scale(xt))
+    ;
+}
 
 // Load Data
 d3.csv("../data/browser.csv", type, function(error, data) {
@@ -85,15 +122,14 @@ d3.csv("../data/browser.csv", type, function(error, data) {
     graph.append("g")
         .attr("class", "axis axis--x")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
+        .call(xAxis);
 
     graph.append("g")
         .attr("class", "axis axis--y")
-        .call(d3.axisLeft(y))
+        .call(yAxis)
         .append("text")
         .attr("transform", "rotate(-90)")
         .attr("y", 6)
-        .attr("dy", "0.71em")
         .attr("fill", "#000")
         .text("%");
 
@@ -114,46 +150,63 @@ d3.csv("../data/browser.csv", type, function(error, data) {
         })
     ;
 
-    // Label
-    browser.append("text")
-        .datum(function(d) {
-            return {
-                name: d.name,
-                value: d.values[d.values.length - 1]};
-        })
-        .attr("transform", function(d) {
-            return "translate(" + x(d.value.date) + "," + y(d.value.number) + ")";
-        })
-        .attr("x", 3)
-        .attr("dy", "0.35em")
-        .style("font", "10px sans-serif")
-        .style("fill", function(d, i) {
-            return colors[i % colors.length];
-        })
-        .text(function(d) {
-            return d.name;
-        })
-        .style('opacity', 0)
-        .transition()
-        .duration(500)
-        .delay(2500)
-        .style('opacity', 1)
-    ;
+    var dateEnd = d3.max(browsers, function(b) {
+            return d3.max(b.values, function(d) {
+                return d.date;
+            })
+        }),
+        dateStart = (new Date(dateEnd)).setYear(dateEnd.getFullYear() - 1);
 
-    // Animations
-    path
-        .attr("stroke-dasharray", function() {
-            var totalLength = d3.select(this).node().getTotalLength();
-            return totalLength + " " + totalLength
-        })
-        .attr("stroke-dashoffset", function() {
-            return d3.select(this).node().getTotalLength();
-        })
-        .transition()
-        .duration(3000)
-        .ease(d3.easeSinInOut)
-        .attr("stroke-dashoffset", 0)
-    ;
+    console.log(dateStart, dateEnd);
+
+    // Gratuitous intro zoom!
+    svg.call(zoom).transition()
+        .duration(1500)
+        .call(zoom.transform, d3.zoomIdentity
+            .scale(width / (x(dateEnd) - x(dateStart)))
+            .translate(-x(dateStart), 0)
+        );
+
+    // // Label
+    // browser.append("text")
+    //     .datum(function(d) {
+    //         return {
+    //             name: d.name,
+    //             value: d.values[d.values.length - 1]};
+    //     })
+    //     .attr("transform", function(d) {
+    //         return "translate(" + x(d.value.date) + "," + y(d.value.number) + ")";
+    //     })
+    //     .attr("x", 3)
+    //     .attr("dy", "0.35em")
+    //     .style("font", "10px sans-serif")
+    //     .style("fill", function(d, i) {
+    //         return colors[i % colors.length];
+    //     })
+    //     .text(function(d) {
+    //         return d.name;
+    //     })
+    //     .style('opacity', 0)
+    //     .transition()
+    //     .duration(500)
+    //     .delay(2500)
+    //     .style('opacity', 1)
+    // ;
+
+    // // Animations
+    // path
+    //     .attr("stroke-dasharray", function() {
+    //         var totalLength = d3.select(this).node().getTotalLength();
+    //         return totalLength + " " + totalLength
+    //     })
+    //     .attr("stroke-dashoffset", function() {
+    //         return d3.select(this).node().getTotalLength();
+    //     })
+    //     .transition()
+    //     .duration(3000)
+    //     .ease(d3.easeSinInOut)
+    //     .attr("stroke-dashoffset", 0)
+    // ;
 });
 
 function type(d, _, columns) {
