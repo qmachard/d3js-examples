4b215210d08a228e122aa29b72b602348fcc6cd3
Quentin MACHARD <quentin.machard@niji.fr>
2017-09-11 15:56:57 +0200

    Example 3 - Entering Elements

--- js/examples/three-little-circles.js
+++ js/examples/three-little-circles.js
@@ -1,13 +1,14 @@
-var circle = d3.selectAll('circle');
+var svg = d3.select('svg');
 
-circle.style('fill', 'steelblue');
+var circle = svg.selectAll('circle')
+    .data([32, 57, 112, 293])
+;
+circle.style("fill", "steelblue");
+circle.attr("r", function(d) { return Math.sqrt(d); });
+circle.attr("cx", function(d, i) { return i * 100 + 30; });
 
-circle.data([32, 57, 112]);
-
-circle.attr('r', function(d) {
-    return Math.sqrt(d);
-});
-
-circle.attr('cx', function(d, i) {
-    return i * 100 + 30;
-});
+var circleEnter = circle.enter().append('circle');
+circleEnter.style("fill", "steelblue");
+circleEnter.attr("cy", 60);
+circleEnter.attr("cx", function(d, i) { return i * 100 + 30; });
+circleEnter.attr("r", function(d) { return Math.sqrt(d); });
