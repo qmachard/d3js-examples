b533eb17f6377c7c8ad56ebee26d2be5dbb40d82
Quentin MACHARD <quentin.machard@niji.fr>
2017-09-11 15:51:32 +0200

    Example 2 - Binding Data

--- js/examples/three-little-circles.js
+++ js/examples/three-little-circles.js
@@ -1,7 +1,13 @@
 var circle = d3.selectAll('circle');
 
 circle.style('fill', 'steelblue');
-circle.attr('r', 30);
-circle.attr('cx', function() {
-    return Math.random() * 710;
+
+circle.data([32, 57, 112]);
+
+circle.attr('r', function(d) {
+    return Math.sqrt(d);
+});
+
+circle.attr('cx', function(d, i) {
+    return i * 100 + 30;
 });
