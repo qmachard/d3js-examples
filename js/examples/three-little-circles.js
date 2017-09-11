5db14cb4a81a8c7d7fbfb9b7a1fc1feec214a6fb
Quentin MACHARD <quentin.machard@niji.fr>
2017-09-11 15:58:03 +0200

    Example 4 - Exiting Elements

--- js/examples/three-little-circles.js
+++ js/examples/three-little-circles.js
@@ -1,14 +1,10 @@
 var svg = d3.select('svg');
 
-var circle = svg.selectAll('circle')
-    .data([32, 57, 112, 293])
-;
+var circle = svg.selectAll("circle")
+    .data([32, 57]);
+
 circle.style("fill", "steelblue");
 circle.attr("r", function(d) { return Math.sqrt(d); });
 circle.attr("cx", function(d, i) { return i * 100 + 30; });
 
-var circleEnter = circle.enter().append('circle');
-circleEnter.style("fill", "steelblue");
-circleEnter.attr("cy", 60);
-circleEnter.attr("cx", function(d, i) { return i * 100 + 30; });
-circleEnter.attr("r", function(d) { return Math.sqrt(d); });
+circle.exit().remove();
